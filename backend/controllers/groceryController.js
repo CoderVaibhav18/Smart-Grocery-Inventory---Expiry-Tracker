9db0658ff6de966055ed74e46e86c15e210b2783
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-16 20:28:08 +0530

    Refactor addItem function

--- backend/controllers/groceryController.js
+++ backend/controllers/groceryController.js
@@ -4,15 +4,19 @@ const getItems = () => {};
 
 const addItem = (req, res) => {
   const { name, quantity, purchase_date, expiry_date } = req.body;
-  
-  db.query("INSERT INTO groceries (name, quantity, purchase_date, expiry_date) VALUES (?, ?, ?, ?)",[name, quantity, purchase_date, expiry_date], async (err, result) => {
-    if (err) {
-      console.log(err);
+
+  db.query(
+    "INSERT INTO groceries (name, quantity, purchase_date, expiry_date) VALUES (?, ?, ?, ?)",
+    [name, quantity, purchase_date, expiry_date],
+    async (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      res.status(200).json({
+        message: "Item added successfully",
+      });
     }
-    res.status(200).json({
-      message: "Item added successfully",
-    })
-  })
+  );
 };
 
 const deleteItem = () => {};
@@ -20,5 +24,5 @@ const deleteItem = () => {};
 module.exports = {
   getItems,
   addItem,
-  deleteItem
-}
+  deleteItem,
+};
