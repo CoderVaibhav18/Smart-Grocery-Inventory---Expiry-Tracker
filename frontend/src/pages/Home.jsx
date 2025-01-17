644167b616f586f94c41e805c5504a6411b05dd0
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-17 13:06:46 +0530

    Form data handle done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,10 +3,11 @@ import "remixicon/fonts/remixicon.css";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  // const navigate = useNavigate();
-
   const submitHandler = (e) => {
     e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
   };
 
   return (
@@ -19,21 +20,25 @@ const Home = () => {
           className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="text"
           placeholder="Name of the grocery"
+          name="name"
         />
         <input
           className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="number"
           placeholder="Grocery quantity"
+          name="quantity"
         />
         <input
           className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="text"
           placeholder="Purchase Date yyyy-mm-dd"
+          name="purchase_date"
         />
         <input
           className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="text"
           placeholder="Expiry Date yyyy-mm-dd"
+          name="expiry_date"
         />
         <button
           type="submit"
