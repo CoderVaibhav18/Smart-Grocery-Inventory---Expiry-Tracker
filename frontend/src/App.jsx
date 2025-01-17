1e89adac057d894550f1d22f68e142e98686a3cb
Vaibhav Sathe <vaibhavsathe160@gmail.com>
2025-01-17 15:03:46 +0530

    Add grocery done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,55 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Feed from "./pages/feed";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="h-screen w-screen">
-      <div className="text-center p-5 flex items-center justify-between">
-        <h3 className="text-2xl font-semibold ">Grocery Inventory</h3>
-        <ul className="flex items-center gap-5  justify-around">
-          <li className="text-xl font-medium">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="text-xl font-medium">
-            <Link to="/feed">Feed</Link>
-          </li>
-        </ul>
-        <h3 className="text-2xl hidden font-bold">
-          <i className="ri-bar-chart-horizontal-line"></i>
-        </h3>
-      </div>
+    <div className="h-screen w-screen ">
+      <nav className="bg-white shadow-md">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center py-5">
+            {/* Logo */}
+            <h3 className="text-2xl font-semibold">Grocery Inventory</h3>
+
+            {/* Desktop Menu */}
+            <ul className=" hidden md:flex items-center gap-8">
+              <li className="text-xl font-medium hover:text-green-600">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="text-xl font-medium hover:text-green-600">
+                <Link to="/feed">Feed</Link>
+              </li>
+            </ul>
+
+            {/* Mobile Menu Button */}
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="md:hidden text-3xl focus:outline-none"
+            >
+              <i className="ri-menu-line"></i>
+            </button>
+          </div>
+
+          {/* Mobile Menu */}
+          {isOpen && (
+            <ul className="md:hidden flex fixed mt-5 text-center  w-full bg-white flex-col gap-4 pb-4">
+              <li className="text-lg font-medium hover:text-green-600">
+                <Link to="/" onClick={() => setIsOpen(false)}>
+                  Home
+                </Link>
+              </li>
+              <li className="text-lg font-medium hover:text-green-600">
+                <Link to="/feed" onClick={() => setIsOpen(false)}>
+                  Feed
+                </Link>
+              </li>
+            </ul>
+          )}
+        </div>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/feed" element={<Feed />} />
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,35 +35,39 @@ const Home = () => {
     <div>
       <form
         onSubmit={(e) => submitHandler(e)}
-        className="p-5 flex mt-3 flex-col gap-5"
+        className="p-5 flex mt-3 flex-col"
       >
+        <h3 className="text-xl font-medium mb-2">Name of grocery</h3>
         <input
-          className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
+          className="py-2 px-8 mb-5 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="text"
           value={groceryName}
           onChange={(e) => setGroceryName(e.target.value)}
           placeholder="Name of the grocery"
         />
+        <h3 className="text-xl font-medium mb-2">Quantity</h3>
         <input
-          className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
+          className="py-2 px-8 mb-5 bg-[#eeee] text-lg font-normal rounded-lg w-full"
           type="number"
           value={quantity}
           onChange={(e) => setQuantity(e.target.value)}
           placeholder="Grocery quantity"
         />
+        <h3 className="text-xl font-medium mb-2">Purchase Date</h3>
         <input
-          className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
-          type="text"
+          className="py-2 px-8 mb-5 bg-[#eeee] text-lg font-normal rounded-lg w-full"
+          type="date"
           value={purchaseDate}
           onChange={(e) => setPurchaseDate(e.target.value)}
-          placeholder="Purchase Date yyyy-mm-dd"
+          placeholder="Purchase Date"
         />
+        <h3 className="text-xl font-medium mb-2">Expiry Data</h3>
         <input
-          className="py-2 px-8 bg-[#eeee] text-lg font-normal rounded-lg w-full"
-          type="text"
+          className="py-2 px-8 mb-5 bg-[#eeee] text-lg font-normal rounded-lg w-full"
+          type="date"
           value={expiryData}
           onChange={(e) => setExpiryData(e.target.value)}
-          placeholder="Expiry Date yyyy-mm-dd"
+          placeholder="Expiry Date"
         />
         <button
           type="submit"
